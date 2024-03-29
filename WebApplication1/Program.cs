3174e85a131c983a92f57c41fd48729aa2038f48
Aldona Zams s16467 <s16467@pjwstk.edu.pl>
2024-03-29 21:01:24 +0100

    Initial project

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,11 @@
+namespace WebApplication1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{Environment.NewLine}Zadanie 1: ");
+        Console.WriteLine("Hello World");
+        
+    }
+}
